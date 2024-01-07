f493045a3264373a3dfb2c8d712990e7d71d6664
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-08 00:49:11 +0200

    Create 101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+ export default function iterateThroughObject(reportWithIterator) {
+  let names = '';
+  for (const name of reportWithIterator) {
+    names += name + ' | ';
+  }
+  return names.slice(0, -3);
+ }
