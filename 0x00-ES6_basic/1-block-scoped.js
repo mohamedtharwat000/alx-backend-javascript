31af326a845e10de9a0e9bab8ee8d2d3425ce7f5
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-07 22:46:01 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
 }
+console.log(taskBlock(true));
+console.log(taskBlock(false));
