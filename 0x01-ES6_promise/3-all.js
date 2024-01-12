8b96bdb86d8ecc4968957dd5a1b1128b48c9c52b
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-12 16:16:39 +0200

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([{ body }, { firstName, lastName }]) => {
-      console.log(body, firstName, lastName);
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
