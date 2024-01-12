4b7dded6c5105bcae05d708eb1aeed535f15dc8d
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-12 15:28:38 +0200

    update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'Success!' });
-    reject('The fake API is not working currently');
+    if (success) return resolve({ status: 200, body: 'Success' });
+    return reject(Error('The fake API is not working currently'));
   });
 }
