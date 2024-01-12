1b91bf00fefbe5b3477e60715e4cba8ac601a10b
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-12 18:06:47 +0200

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    return numerator / denominator;
-  } catch (err) {
-    throw Error('cannot divide by 0');
-  }
+  if (denominator === 0) throw new Error('Division by zero');
+  return numerator / denominator;
 }
