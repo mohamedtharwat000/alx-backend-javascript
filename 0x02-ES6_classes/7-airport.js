4cf528692ad0dcdb287d841dce9f469a08e1b79a
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-13 00:35:07 +0200

    Create 7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
