23d047418056dc591496f4eeabdcbf337164fa09
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-13 00:53:58 +0200

    Create 10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,44 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    if (typeof newBrand !== 'string') {
+      throw TypeError('Brand must be a string');
+    }
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    if (typeof newMotor !== 'string') {
+      throw TypeError('Motor must be a string');
+    }
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    if (typeof newColor !== 'string') {
+      throw TypeError('Color must be a string');
+    }
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    return new Car(this.brand, this.motor, this.color);
+  }
+}
