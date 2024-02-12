9b419d02eb63cb7fd3beba8e7ce8cf9576a9e947
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-13 00:24:03 +0200

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super(currency.code, currency.name);
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw TypeError('Amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
