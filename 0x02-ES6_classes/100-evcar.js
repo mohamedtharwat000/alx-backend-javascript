27e2d5d5ddae75e5af578e2438baf461d403433c
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-13 01:12:55 +0200

    Create 100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new this.constructor.constructor();
+  }
+}
