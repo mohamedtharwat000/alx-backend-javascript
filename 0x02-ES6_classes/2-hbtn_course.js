d9e04623b332362fdbb7b65e360e7476afc5a0e6
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-12 23:55:36 +0200

    Create 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this.students;
+  }
+
+  set students(newStudents) {
+    if (typeof newStudents !== 'object') {
+      throw TypeError('Students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
