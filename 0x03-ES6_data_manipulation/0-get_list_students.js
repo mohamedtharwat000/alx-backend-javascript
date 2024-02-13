5bee0fcef0cbe6cfb15fa37edaaaee6bc0d30741
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-13 22:55:29 +0200

    Create 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'Paris' },
+    { id: 2, firstName: 'John', location: 'London' },
+    { id: 3, firstName: 'Paul', location: 'Paris' },
+  ];
+}
