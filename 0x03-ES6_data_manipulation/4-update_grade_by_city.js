4bbd434d670c0a0728034eb66e4db403dc742f00
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-14 23:03:13 +0200

    fix updateStudentGradeByCity

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,9 @@
-export default function updateStudentGradeByCity(arr = [], city, newGrade) {
+export default function updateStudentGradeByCity(arr = [], city, newGrade = []) {
   return arr
     .filter((student) => student.location === city)
     .map((student) => ({
       ...student,
-      grade: newGrade,
+      grade: (newGrade.find((grade) => grade.studentId === student.id) || {})
+        .grade || 'N/A',
     }));
 }
