7f861ca057b57ed67436202077e3ee72c946e689
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-15 00:32:08 +0200

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   return Array.from(set)
-    .filter((val) => val.startsWith(startString) && startString.length > 0)
+    .filter((val) => val.startsWith(startString) && startString !== '')
     .map((val) => val.slice(startString.length))
     .join('-');
 }
