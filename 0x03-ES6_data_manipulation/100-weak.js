684e5fb1005d44f72fe0be3ed66ede432ca3998b
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-15 01:13:02 +0200

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,2 +1,10 @@
 export const weakMap = new WeakMap();
-export function queryAPI() {}
+export function queryAPI(endpoint) {
+  let endpointNumber = weakMap.get(endpoint);
+  if (endpointNumber) {
+    if (endpointNumber >= 5) throw new Error('Endpoint load is high');
+    weakMap.set(endpoint, (endpointNumber += 1));
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
