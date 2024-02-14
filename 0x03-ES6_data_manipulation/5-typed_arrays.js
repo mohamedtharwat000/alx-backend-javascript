c2ab1798b842e25c25b4b9a219d5cac6ddd63ff3
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-14 23:29:27 +0200

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
   int8View[position] = value;
-  return int8View;
+  return buffer;
 }
