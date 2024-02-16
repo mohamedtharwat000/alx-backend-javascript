6df33d8eb370aaf83becfba1791698b3845860ba
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-16 19:03:29 +0200

    task 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,49 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+type Employee = Director | Teacher;
+type Salary = number | string;
+
+function createEmployee(salary: Salary): Employee {
+  if (Number(salary) < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
