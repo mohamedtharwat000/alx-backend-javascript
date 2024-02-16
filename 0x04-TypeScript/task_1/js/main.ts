5443b9fed1c504dc957accd6d49aed249c9c1bfe
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-16 18:51:29 +0200

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,4 +15,51 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (
+  firstName: string,
+  lastName: string,
+): string => {
+  const firstInitial = firstName.charAt(0);
+  return `${firstInitial}. ${lastName}`;
+};
+
+interface ClassConstructor {
+  readonly firstName: string;
+  readonly lastName: string;
+}
+
+interface ClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements ClassConstructor, ClassInterface {
+  constructor(
+    public readonly firstName: string,
+    public readonly lastName: string,
+  ) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
