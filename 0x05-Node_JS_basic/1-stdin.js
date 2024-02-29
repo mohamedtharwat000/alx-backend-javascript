25e9bbcd9ff527ebf8f128e198f790edae22f9bc
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-29 02:01:07 +0200

    Add stdin.js file for user input and name display

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
