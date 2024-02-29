4184aa3cb60a81ea265f5f839412d7d83cf8ba87
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-29 02:06:45 +0200

    Refactor console.log to process.stdout.write in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
-  const name = data.toString();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
+
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
