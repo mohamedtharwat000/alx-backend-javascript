e37f1e76bc76f93cfce346077f0d97d09881ec1a
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-29 02:47:06 +0200

    Add countStudents function to read and analyze database.csv

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,24 @@
+const { readFileSync, existsSync } = require('node:fs');
+
+function countStudents(path) {
+  if (!existsSync(path)) throw new Error('Cannot load the database');
+  const file = readFileSync(path, 'utf-8');
+  const data = file.split('\n').map((row) => row.split(','));
+  data.pop();
+  data.shift();
+  const studentNumber = data.length;
+  const fields = [];
+  data.forEach((row) => {
+    if (!fields.includes(row[3])) fields.push(row[3]);
+  });
+  console.log(`Number of students: ${studentNumber}`);
+  fields.forEach((field) => {
+    const students = data.filter((row) => row[3] === field);
+    const studentNames = students.map((student) => student[0]).join(', ');
+    console.log(
+      `Number of students in ${field}: ${students.length}. List: ${studentNames}`,
+    );
+  });
+}
+
+countStudents('database.csv');
