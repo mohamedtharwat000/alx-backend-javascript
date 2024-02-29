c0e9f3b445cb5376c73b81f769b823d8d89f0fa5
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-29 03:26:15 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245, null);
 
-// module.exports = app;
+module.exports = app;
