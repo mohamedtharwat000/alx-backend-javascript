a4b5c0bf681fffdf1a195d034e73b86068a1e75f
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-29 02:03:31 +0200

    update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+  const name = data.toString();
   console.log(`Your name is: ${name}`);
   process.exit();
 });
