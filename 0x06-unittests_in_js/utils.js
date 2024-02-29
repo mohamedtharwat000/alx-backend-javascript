282833eb495235155311829a7ad5ff4ec37c2543
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-01 01:38:16 +0200

    Create utils.js

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+        const roundA = Math.round(a);
+        const roundB = Math.round(b);
+
+        switch (type) {
+            case 'SUM':
+                return roundA + roundB;
+            case 'SUBTRACT':
+                return roundA - roundB;
+            case 'DIVIDE':
+                if (roundB === 0) {
+                    return "Error";
+                } else {
+                    return roundA / roundB;
+                }
+            default:
+                return "Error"
+        }
+    }
+}
+
+module.exports = Utils;
