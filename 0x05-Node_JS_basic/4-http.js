9567da3458b758f529f7d3142e2006367afdd03c
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-29 03:25:50 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,12 +2,9 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!\n');
-  res.end();
+  res.end('Hello Holberton School!\n');
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245, null);
 
-module.exports = app;
+// module.exports = app;
